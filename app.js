6d57e47f0763b845cedaf77a92ad3d7b6c68ada2
李留灿 <liucan.li@transsion.com>
2022-08-12 14:32:16 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+App({
+  onLaunch: function () {
+    console.log("Lifecycle App onLaunch");
+  },
+  onShow: function () {
+    console.log("Lifecycle App onShow ddd ");
+  },
+  onHide: function () {
+    console.log("Lifecycle App onHide");
+  },
+});
---
+++ pages/index/index.js
@@ -0,0 +1,5 @@
+Page({
+  data: {
+    text: "this is a blank page",
+  },
+});
